8bc7be9f5b403944eb5cbcc20f59449cdc5fc6ed
Oleg Georgiyevski <oleqg@yahoo.com>
2020-12-22 21:46:10 +0300

    SqlHelper modified

--- src/test/java/ru/netology/data/SqlHelper.java
+++ src/test/java/ru/netology/data/SqlHelper.java
@@ -20,12 +20,12 @@ public class SqlHelper {
     @SneakyThrows
     public static String getPaymentId() {
         String paymentId = null;
-        val idSQL = "SELECT paymentId FROM order_entity order by created desc limit 1;";
+        val idSQL = "SELECT payment_id FROM order_entity order by created desc limit 1;";
         try (val conn = getConnection();
              val statusStmt = conn.prepareStatement(idSQL)) {
             try (val rs = statusStmt.executeQuery()) {
                 if (rs.next()) {
-                    paymentId = rs.getString("paymentId");
+                    paymentId = rs.getString("payment_id");
                 }
             }
         }
@@ -33,12 +33,12 @@ public class SqlHelper {
     }
 
     @SneakyThrows
-    public static String getStatusForPaymentByDebitCard(String payment_id) {
+    public static String getStatusForPaymentByDebitCard(String paymentId) {
         String statusSQL = "SELECT status FROM payment_entity WHERE transaction_id =?; ";
         String status = null;
         try (val conn = getConnection();
              val statusStmt = conn.prepareStatement(statusSQL)) {
-            statusStmt.setString(1, payment_id);
+            statusStmt.setString(1, paymentId);
             try (val rs = statusStmt.executeQuery()) {
                 if (rs.next()) {
                     status = rs.getString("status");
@@ -49,12 +49,12 @@ public class SqlHelper {
     }
 
     @SneakyThrows
-    public static String getPaymentAmount(String payment_id) {
+    public static String getPaymentAmount(String paymentId) {
         String amountSQL = "SELECT amount FROM payment_entity WHERE transaction_id =?; ";
         String amount = null;
         try (val conn = getConnection();
              val statusStmt = conn.prepareStatement(amountSQL)) {
-            statusStmt.setString(1, payment_id);
+            statusStmt.setString(1, paymentId);
             try (val rs = statusStmt.executeQuery()) {
                 if (rs.next()) {
                     amount = rs.getString("amount");
@@ -65,12 +65,12 @@ public class SqlHelper {
     }
 
     @SneakyThrows
-    public static String getStatusForPaymentByCreditCard(String payment_id) {
+    public static String getStatusForPaymentByCreditCard(String paymentId) {
         String statusSQL = "SELECT status FROM credit_request_entity WHERE bank_id =?; ";
         String status = null;
         try (val conn = getConnection();
              val statusStmt = conn.prepareStatement(statusSQL)) {
-            statusStmt.setString(1, payment_id);
+            statusStmt.setString(1, paymentId);
             try (val rs = statusStmt.executeQuery()) {
                 if (rs.next()) {
                     status = rs.getString("status");
